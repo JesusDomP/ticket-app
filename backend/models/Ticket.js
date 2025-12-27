bf924e2e84a62831a4e37fae01cc2e300319433c
JesusDomP <jesus.dominguez0603@gmail.com>
2025-12-26 20:52:20 -0600

    Esquema y validación del ticket

---
+++ backend/models/Ticket.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose#";
+
+const ticketSchema = new mongoose.Schema(
+    // Definición de los campos del esquema y sus validaciones
+    {
+        titulo: { 
+            type: String,
+            required: [true, "El título es obligatorio"],
+            trim: true,
+        },
+
+        descripcion: {
+            type: String, 
+            required: [true, "La descripción es obligatoria"],
+        },
+
+        estado: {
+            type: String,
+            enum: ["Abierto", "En Progreso", "Cerrado"],
+            default: "Abierto",
+        },
+
+        responsable: {
+            type: String, 
+            required: [true, "El responsable es obligatorio"],
+        },
+    },
+    {timestamps: true}    
+);
+
+export default mongoose.model("Ticket", ticketSchema);
